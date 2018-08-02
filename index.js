566e82165f155763db6f20eb0f8fc15bc7addfd2
Jeb Henrie <jebhenrie@gmail.com>
2018-08-02 12:16:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var kittens = ["Milo","Otis","Garfield"]
 
-function destructivelyAppendKitten(){
-  name.push()
-  return name
+function destructivelyAppendKitten(array){
+  array.push()
+  return array
 }
